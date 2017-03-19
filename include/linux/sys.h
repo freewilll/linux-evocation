323209f35924622ee4a2a205a462641bce2e8b59
Will Angenent <will.angenent@ntt.eu>
2017-03-19 17:27:03 +0000

    Tidied up sys.h ...
    - Replaced main syscall TODO with a couple of individual ones
    - Copy pasted the original code & cleaned it up, to rule out various edit
      errors I made previously. The first column is the original real thing now.
    - Added todos for the remaining ones and removed sys_todo.

--- include/linux/sys.h
+++ include/linux/sys.h
@@ -148,158 +148,132 @@ extern int sys_modify_ldt();
 
 typedef int (*fn_ptr)();
 
-
-// TODO WGJA syscalls
 fn_ptr sys_call_table[] = {
-	sys_setup, 			// #define __NR_setup		  0
-	sys_exit, 			// #define __NR_exit		  1
-	sys_fork, 			// #define __NR_fork		  2
-	(fn_ptr) sys_read, 		// #define __NR_read		  3
-	(fn_ptr) sys_write, 		// #define __NR_write		  4
-	(fn_ptr) sys_open, 		// #define __NR_open		  5
-	(fn_ptr) sys_close, 		// #define __NR_close		  6
-	sys_waitpid, 			// #define __NR_waitpid		  7
-	sys_creat, 			// #define __NR_creat		  8
-	sys_link, 			// #define __NR_link		  9
-	sys_unlink, 			// #define __NR_unlink		 10
-	sys_execve, 			// #define __NR_execve		 11
-	sys_chdir, 			// #define __NR_chdir		 12
-	sys_time, 			// #define __NR_time		 13
-	sys_mknod, 			// #define __NR_mknod		 14
-	sys_chmod, 			// #define __NR_chmod		 15
-	sys_chown, 			// #define __NR_chown		 16
-	sys_break, 			// #define __NR_break		 17
-	sys_stat, 			// #define __NR_oldstat		 18
-	sys_lseek, 			// #define __NR_lseek		 19
-	sys_getpid, 			// #define __NR_getpid		 20
-	sys_mount, 			// #define __NR_mount		 21
-	sys_umount, 			// #define __NR_umount		 22
-	sys_setuid, 			// #define __NR_setuid		 23
-	sys_getuid, 			// #define __NR_getuid		 24
-	sys_stime, 			// #define __NR_stime		 25
-	sys_todo, 			// #define __NR_ptrace		 26	todo
-	sys_todo, 			// #define __NR_alarm		 27	todo
-	sys_fstat, 			// #define __NR_oldfstat	 28
-	sys_pause, 			// #define __NR_pause		 29
-	sys_utime, 			// #define __NR_utime		 30
-	sys_stty, 			// #define __NR_stty		 31
-	sys_gtty, 			// #define __NR_gtty		 32
-	sys_access, 			// #define __NR_access		 33
-	(fn_ptr) sys_nice, 		// #define __NR_nice		 34
-	sys_ftime, 			// #define __NR_ftime		 35
-	sys_sync, 			// #define __NR_sync		 36
-	sys_kill, 			// #define __NR_kill		 37
-	sys_rename, 			// #define __NR_rename		 38
-	sys_mkdir, 			// #define __NR_mkdir		 39
-	sys_rmdir, 			// #define __NR_rmdir		 40
-	sys_dup, 			// #define __NR_dup		 41
-	sys_pipe, 			// #define __NR_pipe		 42
-	sys_times, 			// #define __NR_times		 43
-	sys_prof, 			// #define __NR_prof		 44
-	sys_brk, 			// #define __NR_brk		 45
-	sys_setgid, 			// #define __NR_setgid		 46
-	sys_getgid, 			// #define __NR_getgid		 47
-	sys_signal, 			// #define __NR_signal		 48
-	sys_geteuid, 			// #define __NR_geteuid		 49
-	sys_getegid, 			// #define __NR_getegid		 50
-	sys_acct, 			// #define __NR_acct		 51
-	sys_phys, 			// #define __NR_phys		 52
-	sys_lock, 			// #define __NR_lock		 53
-	sys_ioctl, 			// #define __NR_ioctl		 54
-	sys_todo, 			// #define __NR_fcntl		 55	todo
-	sys_mpx, 			// #define __NR_mpx		 56	todo
-	sys_setpgid, 			// #define __NR_setpgid		 57
-	sys_ulimit, 			// #define __NR_ulimit		 58
-	sys_olduname, 			// #define __NR_oldolduname	 59
-	sys_umask, 			// #define __NR_umask		 60
-	sys_chroot, 			// #define __NR_chroot		 61
-	sys_ustat, 			// #define __NR_ustat		 62
-	sys_dup2, 			// #define __NR_dup2		 63
-	sys_getppid, 			// #define __NR_getppid		 64
-	sys_getpgrp, 			// #define __NR_getpgrp		 65
-	sys_setsid, 			// #define __NR_setsid		 66
-	sys_sigaction, 			// #define __NR_sigaction	 67
-	sys_sgetmask, 			// #define __NR_sgetmask	 68
-	sys_ssetmask, 			// #define __NR_ssetmask	 69
-	sys_setreuid, 			// #define __NR_setreuid	 70
-	sys_setregid, 			// #define __NR_setregid	 71
-	sys_sigsuspend, 		// #define __NR_sigsuspend	 72
-	sys_sigpending, 		// #define __NR_sigpending	 73
-	sys_sethostname, 		// #define __NR_sethostname	 74
-	sys_setrlimit, 			// #define __NR_setrlimit	 75
-	sys_getrlimit, 			// #define __NR_getrlimit	 76
-	sys_getrusage, 			// #define __NR_getrusage	 77
-	sys_gettimeofday, 		// #define __NR_gettimeofday	 78
-	sys_settimeofday, 		// #define __NR_settimeofday	 79
-	sys_getgroups,			// #define __NR_getgroups	 80
-	sys_setgroups, 			// #define __NR_setgroups	 81
-	sys_todo, 			// #define __NR_select		 82	todo
-	sys_symlink, 			// #define __NR_symlink		 83
-	sys_lstat, 			// #define __NR_oldlstat	 84
-	sys_readlink, 			// #define __NR_readlink	 85
-	sys_todo, 			// #define __NR_uselib		 86	todo
-	sys_todo, 			// #define __NR_swapon		 87	todo
-	sys_reboot, 			// #define __NR_reboot		 88
-	sys_readdir, 			// #define __NR_readdir		 89
-	sys_mmap, 			// #define __NR_mmap		 90
-	sys_munmap, 			// #define __NR_munmap		 91
-	sys_truncate, 			// #define __NR_truncate	 92
-	sys_ftruncate, 			// #define __NR_ftruncate	 93
-	sys_fchmod, 			// #define __NR_fchmod		 94
-	sys_fchown, 			// #define __NR_fchown		 95
-	sys_getpriority, 		// #define __NR_getpriority	 96
-	sys_setpriority, 		// #define __NR_setpriority	 97
-	sys_profil, 			// #define __NR_profil		 98
-	sys_statfs, 			// #define __NR_statfs		 99
-	sys_fstatfs, 			// #define __NR_fstatfs		100
-	sys_ioperm, 			// #define __NR_ioperm		101
-	sys_todo, 			// #define __NR_socketcall	102	todo
-	sys_todo, 			// #define __NR_syslog		103	todo
-	sys_todo, 			// #define __NR_setitimer	104	todo
-	sys_todo, 			// #define __NR_getitimer	105	todo
-	sys_newstat, 			// #define __NR_stat		106
-	sys_newlstat, 			// #define __NR_lstat		107
-	sys_newfstat, 			// #define __NR_fstat		108
-	sys_olduname,			// #define __NR_olduname	109
-	sys_iopl, 			// #define __NR_iopl		110
-	sys_vhangup, 			// #define __NR_vhangup		111
-	sys_idle, 			// #define __NR_idle		112
-	sys_vm86, 			// #define __NR_vm86		113
-	sys_wait4, 			// #define __NR_wait4		114
-	sys_todo, 			// #define __NR_swapoff		115	todo
-	sys_todo, 			// #define __NR_sysinfo		116	todo
-	sys_todo, 			// #define __NR_ipc		117	todo
-	sys_fsync, 			// #define __NR_fsync		118
-	sys_sigreturn, 			// #define __NR_sigreturn	119
-	sys_clone, 			// #define __NR_clone		120
-	sys_setdomainname, 		// #define __NR_setdomainname	121
-	sys_uname, 			// #define __NR_uname		122
-	sys_todo, 			// #define __NR_modify_ldt	123	todo
+	sys_setup,		// __NR_setup		0
+	sys_exit,		// __NR_exit		1
+	sys_fork,		// __NR_fork		2
+	(fn_ptr) sys_read,	// __NR_read		3
+	(fn_ptr) sys_write,	// __NR_write		4
+	(fn_ptr) sys_open,	// __NR_open		5
+	(fn_ptr) sys_close,	// __NR_close		6
+	sys_waitpid,		// __NR_waitpid		7
+	sys_creat,		// __NR_creat		8
+	sys_link,		// __NR_link		9
+	sys_unlink,		// __NR_unlink		10
+	sys_execve,		// __NR_execve		11
+	sys_chdir,		// __NR_chdir		12
+	sys_time,		// __NR_time		13
+	sys_mknod,		// __NR_mknod		14
+	sys_chmod,		// __NR_chmod		15
+	sys_chown,		// __NR_chown		16
+	sys_break,		// __NR_break		17
+	sys_stat,		// __NR_oldstat		18
+	sys_lseek,		// __NR_lseek		19
+	sys_getpid,		// __NR_getpid		20
+	sys_mount,		// __NR_mount		21
+	sys_umount,		// __NR_umount		22
+	sys_setuid,		// __NR_setuid		23
+	sys_getuid,		// __NR_getuid		24
+	sys_stime,		// __NR_stime		25
+	sys_ptrace,		// __NR_ptrace		26	todo
+	sys_alarm,		// __NR_alarm		27	todo
+	sys_fstat,		// __NR_oldfstat	28
+	sys_pause,		// __NR_pause		29
+	sys_utime,		// __NR_utime		30
+	sys_stty,		// __NR_stty		31
+	sys_gtty,		// __NR_gtty		32
+	sys_access,		// __NR_access		33
+	(fn_ptr) sys_nice,	// __NR_nice		34
+	sys_ftime,		// __NR_ftime		35
+	sys_sync,		// __NR_sync		36
+	sys_kill,		// __NR_kill		37
+	sys_rename,		// __NR_rename		38
+	sys_mkdir,		// __NR_mkdir		39
+	sys_rmdir,		// __NR_rmdir		40
+	sys_dup,		// __NR_dup		41
+	sys_pipe,		// __NR_pipe		42
+	sys_times,		// __NR_times		43
+	sys_prof,		// __NR_prof		44
+	sys_brk,		// __NR_brk		45
+	sys_setgid,		// __NR_setgid		46
+	sys_getgid,		// __NR_getgid		47
+	sys_signal,		// __NR_signal		48
+	sys_geteuid,		// __NR_geteuid		49
+	sys_getegid,		// __NR_getegid		50
+	sys_acct,		// __NR_acct		51
+	sys_phys,		// __NR_phys		52
+	sys_lock,		// __NR_lock		53
+	sys_ioctl,		// __NR_ioctl		54
+	sys_fcntl,		// __NR_fcntl		55	todo
+	sys_mpx,		// __NR_mpx		56	todo
+	sys_setpgid,		// __NR_setpgid		57
+	sys_ulimit,		// __NR_ulimit		58
+	sys_olduname,		// __NR_oldolduname	59
+	sys_umask,		// __NR_umask		60
+	sys_chroot,		// __NR_chroot		61
+	sys_ustat,		// __NR_ustat		62
+	sys_dup2,		// __NR_dup2		63
+	sys_getppid,		// __NR_getppid		64
+	sys_getpgrp,		// __NR_getpgrp		65
+	sys_setsid,		// __NR_setsid		66
+	sys_sigaction,		// __NR_sigaction	67
+	sys_sgetmask,		// __NR_sgetmask	68
+	sys_ssetmask,		// __NR_ssetmask	69
+	sys_setreuid,		// __NR_setreuid	70
+	sys_setregid,		// __NR_setregid	71
+	sys_sigsuspend,		// __NR_sigsuspend	72
+	sys_sigpending,		// __NR_sigpending	73
+	sys_sethostname,	// __NR_sethostname	74
+	sys_setrlimit,		// __NR_setrlimit	75
+	sys_getrlimit,		// __NR_getrlimit	76
+	sys_getrusage,		// __NR_getrusage	77
+	sys_gettimeofday,	// __NR_gettimeofday	78
+	sys_settimeofday,	// __NR_settimeofday	79
+	sys_getgroups,		// __NR_getgroups	80
+	sys_setgroups,		// __NR_setgroups	81
+	sys_select,		// __NR_select		82	todo
+	sys_symlink,		// __NR_symlink		83
+	sys_lstat,		// __NR_oldlstat	84
+	sys_readlink,		// __NR_readlink	85
+	sys_uselib,		// __NR_uselib		86	todo
+	sys_swapon,		// __NR_swapon		87	todo
+	sys_reboot,		// __NR_reboot		88
+	sys_readdir,		// __NR_readdir		89
+	sys_mmap,		// __NR_mmap		90
+	sys_munmap,		// __NR_munmap		91
+	sys_truncate,		// __NR_truncate	92
+	sys_ftruncate,		// __NR_ftruncate	93
+	sys_fchmod,		// __NR_fchmod		94
+	sys_fchown,		// __NR_fchown		95
+	sys_getpriority,	// __NR_getpriority	96
+	sys_setpriority,	// __NR_setpriority	97
+	sys_profil,		// __NR_profil		98
+	sys_statfs,		// __NR_statfs		99
+	sys_fstatfs,		// __NR_fstatfs		100
+	sys_ioperm,		// __NR_ioperm		101
+	sys_socketcall,		// __NR_socketcall	102	todo
+	sys_syslog,		// __NR_syslog		103	todo
+	sys_setitimer,		// __NR_setitimer	104	todo
+	sys_getitimer,		// __NR_getitimer	105	todo
+	sys_newstat,		// __NR_stat		106
+	sys_newlstat,		// __NR_lstat		107
+	sys_newfstat,		// __NR_fstat		108
+	sys_uname,		// __NR_olduname	109
+	sys_iopl,		// __NR_iopl		110
+	sys_vhangup,		// __NR_vhangup		111
+	sys_idle,		// __NR_idle		112
+	sys_vm86,		// __NR_vm86		113
+	sys_wait4,		// __NR_wait4		114
+	sys_swapoff,		// __NR_swapoff		115	todo
+	sys_sysinfo,		// __NR_sysinfo		116	todo
+	sys_ipc,		// __NR_ipc		117	todo
+	sys_fsync,		// __NR_fsync		118
+	sys_sigreturn,		// __NR_sigreturn	119
+	sys_clone,		// __NR_clone		120
+	sys_setdomainname,	// __NR_setdomainname	121
+	sys_newuname,		// __NR_uname		122
+	sys_modify_ldt,		// __NR_modify_ldt	123	todo
 };
-
-// TODO WGJA WIP: fn_ptr sys_call_table[] = { sys_setup, sys_exit, sys_fork, sys_read,
-// TODO WGJA WIP: sys_write, sys_open, sys_close, sys_waitpid, sys_creat, sys_link,
-// TODO WGJA WIP: sys_unlink, sys_execve, sys_chdir, sys_time, sys_mknod, sys_chmod,
-// TODO WGJA WIP: sys_chown, sys_break, sys_stat, sys_lseek, sys_getpid, sys_mount,
-// TODO WGJA WIP: sys_umount, sys_setuid, sys_getuid, sys_stime, sys_ptrace, sys_alarm,
-// TODO WGJA WIP: sys_fstat, sys_pause, sys_utime, sys_stty, sys_gtty, sys_access,
-// TODO WGJA WIP: sys_nice, sys_ftime, sys_sync, sys_kill, sys_rename, sys_mkdir,
-// TODO WGJA WIP: sys_rmdir, sys_dup, sys_pipe, sys_times, sys_prof, sys_brk, sys_setgid,
-// TODO WGJA WIP: sys_getgid, sys_signal, sys_geteuid, sys_getegid, sys_acct, sys_phys,
-// TODO WGJA WIP: sys_lock, sys_ioctl, sys_fcntl, sys_mpx, sys_setpgid, sys_ulimit,
-// TODO WGJA WIP: sys_olduname, sys_umask, sys_chroot, sys_ustat, sys_dup2, sys_getppid,
-// TODO WGJA WIP: sys_getpgrp, sys_setsid, sys_sigaction, sys_sgetmask, sys_ssetmask,
-// TODO WGJA WIP: sys_setreuid,sys_setregid, sys_sigsuspend, sys_sigpending,
-// TODO WGJA WIP: sys_sethostname, sys_setrlimit, sys_getrlimit, sys_getrusage,
-// TODO WGJA WIP: sys_gettimeofday, sys_settimeofday, sys_getgroups, sys_setgroups,
-// TODO WGJA WIP: sys_select, sys_symlink, sys_lstat, sys_readlink, sys_uselib,
-// TODO WGJA WIP: sys_swapon, sys_reboot, sys_readdir, sys_mmap, sys_munmap, sys_truncate,
-// TODO WGJA WIP: sys_ftruncate, sys_fchmod, sys_fchown, sys_getpriority, sys_setpriority,
-// TODO WGJA WIP: sys_profil, sys_statfs, sys_fstatfs, sys_ioperm, sys_socketcall,
-// TODO WGJA WIP: sys_syslog, sys_setitimer, sys_getitimer, sys_newstat, sys_newlstat,
-// TODO WGJA WIP: sys_newfstat, sys_uname, sys_iopl, sys_vhangup, sys_idle, sys_vm86,
-// TODO WGJA WIP: sys_wait4, sys_swapoff, sys_sysinfo, sys_ipc, sys_fsync, sys_sigreturn,
-// TODO WGJA WIP: sys_clone, sys_setdomainname, sys_newuname, sys_modify_ldt};
 
 }
 
--- will/todo.c
+++ will/todo.c
@@ -4,19 +4,132 @@
 #include <linux/tty.h>
 #include <linux/user.h>
 
-// TODO WGJA syscall_trace
-extern "C" void syscall_trace(void)
+// TODO WGJA syscall_trace 
+extern "C" int syscall_trace(void)
 {
 	printk("TODO: syscall_trace\n");
-	for (;;);
-}
-
-// TODO WGJA syscalls
-extern "C" int sys_todo(struct pt_regs regs)
-{
-	printk("todo syscall %#x (%d).\n", regs.orig_eax, regs.orig_eax);
 	return -ENOSYS;
 }
+
+// TODO WGJA sys_ptrace 
+extern "C" int sys_ptrace(void)
+{
+	printk("TODO: sys_ptrace\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_alarm 
+extern "C" int sys_alarm(void)
+{
+	printk("TODO: sys_alarm\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_fcntl 
+extern "C" int sys_fcntl(void)
+{
+	printk("TODO: sys_fcntl\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_select 
+extern "C" int sys_select(void)
+{
+	printk("TODO: sys_select\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_uselib 
+extern "C" int sys_uselib(void)
+{
+	printk("TODO: sys_uselib\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_swapon 
+extern "C" int sys_swapon(void)
+{
+	printk("TODO: sys_swapon\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_socketcall 
+extern "C" int sys_socketcall(void)
+{
+	printk("TODO: sys_socketcall\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_setitimer 
+extern "C" int sys_setitimer(void)
+{
+	printk("TODO: sys_setitimer\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_getitimer 
+extern "C" int sys_getitimer(void)
+{
+	printk("TODO: sys_getitimer\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_swapoff 
+extern "C" int sys_swapoff(void)
+{
+	printk("TODO: sys_swapoff\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_sysinfo 
+extern "C" int sys_sysinfo(void)
+{
+	printk("TODO: sys_sysinfo\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_ipc 
+extern "C" int sys_ipc(void)
+{
+	printk("TODO: sys_ipc\n");
+	return -ENOSYS;
+}
+
+// TODO WGJA sys_modify_ldt 
+extern "C" int sys_modify_ldt(void)
+{
+	printk("TODO: sys_modify_ldt\n");
+	return -ENOSYS;
+}
+
+
+// kernel/kernel.o:(.data+0xb48): undefined reference to `sys_ptrace'
+// kernel/kernel.o:(.data+0xb4c): undefined reference to `sys_alarm'
+// kernel/kernel.o:(.data+0xbbc): undefined reference to `sys_fcntl'
+// kernel/kernel.o:(.data+0xc28): undefined reference to `sys_select'
+// kernel/kernel.o:(.data+0xc38): undefined reference to `sys_uselib'
+// kernel/kernel.o:(.data+0xc3c): undefined reference to `sys_swapon'
+// kernel/kernel.o:(.data+0xc78): undefined reference to `sys_socketcall'
+// kernel/kernel.o:(.data+0xc80): undefined reference to `sys_setitimer'
+// kernel/kernel.o:(.data+0xc84): undefined reference to `sys_getitimer'
+// kernel/kernel.o:(.data+0xcac): undefined reference to `sys_swapoff'
+// kernel/kernel.o:(.data+0xcb0): undefined reference to `sys_sysinfo'
+// kernel/kernel.o:(.data+0xcb4): undefined reference to `sys_ipc'
+// kernel/kernel.o:(.data+0xccc): undefined reference to `sys_modify_ldt'
+
+// // TODO WGJA syscall_trace
+// extern "C" void syscall_trace(void)
+// {
+// 	printk("TODO: syscall_trace\n");
+// 	for (;;);
+// }
+
+// // TODO WGJA syscalls
+// extern "C" int sys_todo(struct pt_regs regs)
+// {
+// 	printk("todo syscall %#x (%d).\n", regs.orig_eax, regs.orig_eax);
+// 	return -ENOSYS;
+// }
 
 // TODO WGJA move sound_mem_init to real thing
 void sound_mem_init(void)
